beed51961ecb2371a834338a8a98d0d9f9b473a7
jinyongp <dev.jinyongp@gmail.com>
2023-09-18 16:12:49 +0900

    init: 프로젝트 생성 및 초기화

---
+++ NbcampSNS/Controllers/ViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initializeUI()
+    }
+
+    private func initializeUI() {
+        view.backgroundColor = .systemBackground
+    }
+}
